a42a054bd366dd075299f145dc8918906a2baecc
rajendragosavi <raje.g.995@gmail.com>
2025-03-13 16:19:02 +0530

    addded bye handler

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("Running Webserver on port 8080")
-    http.HandleFunc("/hello", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("Running Webserver on port 8080")
+	http.HandleFunc("/hello", HelloServer)
+	http.HandleFunc("/bye", ByeServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func ByeServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bye, %s!", r.URL.Path[1:])
 }
